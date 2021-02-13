1794cf45f00eb63b40afc82170343f6bf29e73b0
Michal Wozniak <michal.wozniak@viviclabs.com>
2021-02-13 14:29:39 +0100

    Git info.

--- deploy.sh
+++ deploy.sh
@@ -8,17 +8,17 @@ cd ..
 cp -r capser_v2_frontend/build/. backend/src/main/resources/static
 mkdir temp
 cp -r backend/. temp/
-ls
+
 
 cd temp || exit
 git init
 git add .
 git commit -m "Deploy"
 
- git config user.email "michael93509@gmail.com"
- git config user.name "Michal Wozniak"
+git config user.email "michael93509@gmail.com"
+git config user.name "Michal Wozniak"
 
-HEROKU_API_KEY="$HEROKU_API_KEY" heroku git:remote -a capser
+heroku git:remote -a capser
 git push heroku master -f
 cd ..
 rm -r -f temp
