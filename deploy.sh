8f81404c35a67b51f6bff569e66d2078243b4510
AeTher <michael93509@gmail.com>
2021-02-13 13:25:32 +0100

    Update deploy.sh
    Login before cd

--- deploy.sh
+++ deploy.sh
@@ -15,12 +15,12 @@ cd ..
 cp -r capser_v2_frontend/build/. backend/src/main/resources/static
 mkdir temp
 cp -r backend/. temp/
+expect heroku_login.ex
 cd temp || exit
 git init
 git add .
 git commit -m "Deploy"
 
-expect heroku_login.ex
 heroku git:remote -a capser
 git push heroku master -f
 cd ..
