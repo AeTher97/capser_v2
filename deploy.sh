ef3ff4141b0957a3cfe20b49d7d0d6de81cdd633
Michal Wozniak <michal.wozniak@viviclabs.com>
2021-02-13 15:11:56 +0100

    Modify global netrc

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 cd capser_v2_frontend || exit
-npm install
-npm audit fix
-CI='' npm run build
+#npm install
+#npm audit fix
+#CI='' npm run build
 cd ..
 cp -r capser_v2_frontend/build/. backend/src/main/resources/static
 mkdir temp
@@ -14,6 +14,7 @@ cd temp || exit
 git init
 git add .
 git commit -m "Deploy"
+read API_KEY
 
 git config user.email "michael93509@gmail.com"
 git config user.name "Michal Wozniak"
@@ -23,7 +24,7 @@ echo "machine api.heroku.com
   password $API_KEY
 machine git.heroku.com
   login michael93509@gmail.com
-  password $API_KEY" > /.netrc
+  password $API_KEY" > ~/.netrc
 
 heroku git:remote -a capser
 git push heroku master -f
