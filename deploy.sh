6bcc7e0d62e2b93f5980de24d2c5443da82a6ce4
Michal Wozniak <michal.wozniak@viviclabs.com>
2021-02-13 16:55:10 +0100

    Master

--- deploy.sh
+++ deploy.sh
@@ -12,15 +12,17 @@ cp .netrc ~/
 
 
 cd temp || exit
+
+
+git config user.email "michael93509@gmail.com"
+git config user.name "Michal Wozniak"
 git init
 git add .
 git commit -m "Deploy"
 
 
-git config user.email "michael93509@gmail.com"
-git config user.name "Michal Wozniak"
 
-git push -f https://heroku:${HEROKU_API_TOKEN}@git.heroku.com/capser.git origin/master:master
+git push -f https://heroku:${HEROKU_API_TOKEN}@git.heroku.com/capser.git master
 cd ..
 rm -r -f temp
 
