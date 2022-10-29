993308420ffd7757c11a06d076a2ffde07da62bf
Michał Woźniak <michael93509@gmail.com>
2022-10-29 22:10:38 +0200

    Fix random parenthesis close in headers.

--- frontend/src/components/misc/PageHeader.jsx
+++ frontend/src/components/misc/PageHeader.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import {Typography, useTheme} from "@material-ui/core";
 import mainStyles from "../../misc/styles/MainStyles";
 
-const PageHeader = ({title, font, showLogo, icon, noSpace, fontWeight = 400}) => {
+const PageHeader = ({title, font, showLogo, icon, fontWeight = 400}) => {
     const classes = mainStyles();
     const theme = useTheme();
 
@@ -13,7 +13,7 @@ const PageHeader = ({title, font, showLogo, icon, noSpace, fontWeight = 400}) =>
              className={[classes.header].join(' ')}>
             {icon && <div style={{padding: 10, color: 'white'}}>{icon}</div>}
             {showLogo ? <img src={"/logo192.png"} style={{maxWidth: 80, padding: 10}}/> :
-                <div style={{minHeight: 100}}/>}}
+                <div style={{minHeight: 100}}/>}
             <Typography variant={"h4"} style={style}>{title}</Typography>
         </div>
     );
